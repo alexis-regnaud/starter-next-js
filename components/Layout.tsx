3dbaac82554e40ab255f1a02b08a35aa9273864a
alex83130 <Alex-83130>
2020-05-24 09:10:41 -0400

    update

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,13 +2,13 @@
 
 import React from "react";
 import Head from "next/head";
-import Link from "next/link";
 import { destroyCookie } from "nookies";
-import Button from "@material-ui/core/Button";
+import { Container } from "@material-ui/core";
+import Navigation from "./Navigation";
 
 export default function Layout({ children }) {
   const handleSignOut = () => {
-    destroyCookie("", "token");
+    //  destroyCookie("", "token");
     window.location.reload();
   };
 
@@ -19,33 +19,9 @@ export default function Layout({ children }) {
         <title>{title}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link
-          rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-          crossOrigin="anonymous"
-        />
-        <script src="https://js.stripe.com/v3" />
       </Head>
-      <header>
-        <div>
-          <Link href="/">
-            <a className="navbar-brand">Home</a>
-          </Link>
-
-          <Link href="/signin">
-            <a className="nav-link">Sign In</a>
-          </Link>
-
-          <Link href="/signup">
-            <a className="nav-link"> Sign Up</a>
-          </Link>
-          <Button variant="contained" color="primary" onClick={handleSignOut}>
-            Sign Out
-          </Button>
-        </div>
-      </header>
-      <div>{children}</div>
+      <Navigation />
+      <Container>{children}</Container>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,25 @@
 /* pages/index.js */
 import React, { useState } from "react";
 import styled from "styled-components";
+import gql from "graphql-tag";
+import test from "../components/test.graphql";
+import Layout from "../components/Layout";
+
+const query = gql`
+  {
+    restaurants {
+      id
+    }
+  }
+`;
 
 export default function Home() {
   return (
-    <div className="container-fluid">
-      <Title>My First Next.js Page</Title>
-    </div>
+    <Layout>
+      <div className="container-fluid">
+        <Title>My First Next.js Page</Title>
+      </div>
+    </Layout>
   );
 }
 
